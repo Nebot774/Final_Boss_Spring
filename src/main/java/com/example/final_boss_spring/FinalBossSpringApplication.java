85a49ebe33b6f39b8f646e46173a37f34ee4daa0
Nebot774 <adriclemenbot@gmail.com>
2024-04-20 13:19:40 +0200

    Commit:subiendo el proyecto al github

---
+++ src/main/java/com/example/final_boss_spring/FinalBossSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.final_boss_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalBossSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalBossSpringApplication.class, args);
+    }
+
+}
